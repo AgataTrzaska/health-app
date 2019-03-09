3a646de15144aac50b5378bd790c3dca436d8e0c
AstralEden <agataa.trzaska@gmail.com>
2019-03-09 18:05:09 +0100

    init

---
+++ src/main/java/pl/mybrand/Bmi_Cont/BmiApplication.java
@@ -0,0 +1,14 @@
+package pl.mybrand.Bmi_Cont;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class BmiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BmiApplication.class, args);
+}
+
+}
